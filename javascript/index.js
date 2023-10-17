78be345a77043095e3cfbfad461ccf6cf28f4ef2
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-10-16 18:26:05 -0600

    fetch request to index working, need to parse to HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,7 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function dropdownFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    console.log("clicked");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+// const liturgicalButton = document.getElementById("liturgicalButton");
+
+async function liturgicalClick(){
+  const response = await fetch("http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today");
+  const data = await response.json();
+  console.log(data);
+}
