57e13c796383f4fdc497e5219a2ca9bfbffe5fdd
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-11-01 16:17:16 -0600

    stylized liturgical table and aboutme, need to center profile picture

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,9 @@
 const tbody = document.getElementById("tbody");
 
 async function liturgicalClick(){
+  document.getElementById("liturgicalButton").onclick=null;
   const response = await fetch("http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today");
   const data = await response.json();
-  console.log(data);
-  console.log(data.date);
-  console.log(data.season);
-  console.log(data.season_week);
-  console.log(data.celebrations[0].title);
   tbody.innerHTML += `<tr>
         <td>${data.date}</td>
         <td>${data.season}</td>
